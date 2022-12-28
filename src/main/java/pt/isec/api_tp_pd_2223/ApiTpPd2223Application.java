e665c417c9cc5f941cf92a87d21042b986924a26
João Rocha <jmarocha494@gmail.com>
2022-12-28 22:49:07 +0000

    Initial commit

---
+++ src/main/java/pt/isec/api_tp_pd_2223/ApiTpPd2223Application.java
@@ -0,0 +1,13 @@
+package pt.isec.api_tp_pd_2223;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTpPd2223Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTpPd2223Application.class, args);
+    }
+
+}
---
+++ src/test/java/pt/isec/api_tp_pd_2223/ApiTpPd2223ApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.isec.api_tp_pd_2223;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTpPd2223ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
